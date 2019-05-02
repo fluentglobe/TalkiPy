5db6466018df8a8ff14a466123cc67fb2c8ae0c8
Henrik Vendelbo <henrik@thepia.com>
2019-05-02 23:39:21 +0200

    ESP32 build

---
+++ ports/esp32/config.sh
@@ -0,0 +1,5 @@
+export PORT=/dev/cu.usbserial-DN050JF3
+export PATH=/usr/local/opt/ccache/libexec:/Volumes/Projects/TalkiPy/ports/esp32/esp-idf/tools:/Volumes/Projects/xtensa-esp32-elf/bin:$PATH
+export IDF_PATH=/Volumes/Projects/TalkiPy/ports/esp32/esp-idf
+export ESPIDF=/Volumes/Projects/TalkiPy/ports/esp32/esp-idf
+export ADF_PATH=/Volumes/Projects/esp-adf
